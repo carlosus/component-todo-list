b28be1e3fb3f6e7154aa257fbe1d8c6f6b7585db
Carlos Us Santiago <cdus12@gmail.com>
2019-05-22 10:32:46 -0700

    set up components. set up header to display on the page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import Component from './Component.js';
 
 class Header extends Component {
-    
+    renderTemplate() {
+        return /*html*/ `
+            <header>
+                <h1>Todo List</h1>
+            </header>
+        `;
+    }
 }
 
 export default Header;
